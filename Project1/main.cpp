0b3f3affbecc87870080b486d1a311c83a7c9be3
unknown <1094050886@qq.com>
2019-03-09 09:53:06 +0800

    完成imgui修改三角形颜色

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,6 +3,10 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <stdio.h>
 using namespace std;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -10,59 +14,58 @@ void processInput(GLFWwindow * window);
 bool readFile(const string & fileName, string & content);
 void checkCompile(const int & shader, const int & checkType);
 void compileShader(unsigned int & shader, const char * filename, const int & shaderType);
+static void glfw_error_callback(int error, const char* description)
+{
+	fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
 // 着色器文件
 const char* vertexShaderFile = "shader.vs";
 const char* fragmentShaderFile = "shader.fs";
 
+const char* glsl_version = "#version 130";
 
 int main() {
+	// Setup window
+	glfwSetErrorCallback(glfw_error_callback);
+	if (!glfwInit())
+		return 1;
 	// 初始化GLFW
 	glfwInit();
 	// 设置GLFW - OpenGL 3.3 core mode
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);	
 	// 创建窗口
-	GLFWwindow* window = glfwCreateWindow(400, 400, "test", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "test", NULL, NULL);
 	if (window == NULL) {
 		cout << "FAIL" << endl;
 		glfwTerminate();
 		return -1;
 	}
-
 	// 将创建的窗口的上下文设为当前线程的主上下文
 	glfwMakeContextCurrent(window);
-	// 创建视口
-	//glViewport(0, 0, 200, 300);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
+	glfwSwapInterval(1); // Enable vsync
 	// 初始化GLAD，加载系统相关OpenGL函数指针
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		cout << "FAIL" << endl;
 		return -1;
 	}
-	
-	// --------------- 绘制三角形 ---------------
-
-	// 指定三角形顶点
-	GLfloat vertices[] = {
-		 0,  1, 0, 1, 0, 0,
-		-1, -1, 0, 0, 1, 0,
-		 1, -1, 0, 0, 0, 1 };
-	// VBO
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	// 绑定缓冲
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	// 把之前定义的顶点（float数组）复制到缓冲的内存
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	// --------------- 顶点着色器 --------------- 
+	// set up GUI context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+	// Setup Platform/Renderer bindings
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+	ImVec4 clear_color = ImVec4(0, 0, 0, 1.00f);
+	ImVec4 triangleColor = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+	// --------------- 编译顶点、片段着色器 --------------- 
 	unsigned int vertexShader;
 	compileShader(vertexShader, vertexShaderFile, 1);
-	// --------------- 片段着色器 --------------- 
 	unsigned int fragmentShader;
 	compileShader(fragmentShader, fragmentShaderFile, 2);
 	// --------------- 着色器程序 --------------- 
@@ -76,14 +79,14 @@ int main() {
 	checkCompile(shaderProgram, 2);
 	// 激活程序对象
 	glUseProgram(shaderProgram);
-
-	// --------------- 顶点数组对象 --------------- 
-	unsigned int VAO;
+	// VBO, VAO
+	unsigned int VBO;
+	unsigned int VAO; // 顶点数组对象 
+	glGenBuffers(1, &VBO);
 	glGenVertexArrays(1, &VAO);
-	// 绑定
+	// 绑定缓冲
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBindVertexArray(VAO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
 	// --------------- 链接顶点属性 --------------- 
 	// 位置属性
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), 0);
@@ -99,26 +102,50 @@ int main() {
 	while (!glfwWindowShouldClose(window)) {
 		// 检查触发事件、更新窗口，回调
 		glfwPollEvents();
+		// ----------------- imGUI ----------------- 
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		// 颜色选择窗口细节
+		ImGui::Begin("Color Setting");
+		ImGui::ColorEdit3("Triangle color", (float*)&triangleColor);
+		ImGui::End();
+		// Rendering
+		ImGui::Render();
+		int display_w, display_h;
+		glfwMakeContextCurrent(window);
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		// ----------------- imGUI ----------------- 
+		// ----------------- Triangle ----------------- 
+		// 指定三角形顶点和颜色
+		GLfloat vertices[] = {
+			 0,  1, 0, triangleColor.x, triangleColor.y, triangleColor.z,
+			-1, -1, 0, triangleColor.x, triangleColor.y, triangleColor.z,
+			 1, -1, 0, triangleColor.x, triangleColor.y, triangleColor.z };
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 		// processInput(window);
 		// 渲染
 		// 清屏颜色
-		glClearColor(0, 0, 0, 1);
+		// glClearColor(0.5, 0.5, 0.5, 1);
 		// 清屏
-		glClear(GL_COLOR_BUFFER_BIT);
+		// glClear(GL_COLOR_BUFFER_BIT);
 		// 绘制
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glBindVertexArray(0);
-
+		glViewport(0, 0, 1280, 720);
+		glfwMakeContextCurrent(window);
 		// 交换缓冲、绘制、显示
 		glfwSwapBuffers(window);
-		
-
 	}
 
 	// 释放/删除资源
-	
 	glfwTerminate();
 	return 0;
 }
